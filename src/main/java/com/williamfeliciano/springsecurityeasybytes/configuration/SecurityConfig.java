ba06526cae6850372f98db33f26d37d03a492a8d
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-10-24 21:50:45 -0400

    completed Secction7

--- src/main/java/com/williamfeliciano/springsecurityeasybytes/configuration/SecurityConfig.java
+++ src/main/java/com/williamfeliciano/springsecurityeasybytes/configuration/SecurityConfig.java
@@ -4,7 +4,6 @@ package com.williamfeliciano.springsecurityeasybytes.configuration;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,22 +21,23 @@ public class SecurityConfig  {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.securityContext().requireExplicitSave(false)
-                .and().cors().configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
-        }).and().csrf().ignoringRequestMatchers("/contact","/register")
+                .and().cors().configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }).and().csrf().ignoringRequestMatchers("/contact","/register")
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and().authorizeHttpRequests()
-                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards", "/user")
-                .authenticated().requestMatchers("/notices","/contact","/register").permitAll()
+                .requestMatchers("/myAccount").hasRole("USER")
+                .requestMatchers("/myBalance").hasAnyRole("USER","ADMIN")
+                .requestMatchers("/myLoans").hasRole("USER")
+                .requestMatchers("/myCards").hasRole("USER")
+                .requestMatchers("/user").authenticated()
+                .requestMatchers("/notices","/contact","/register").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
--- src/main/java/com/williamfeliciano/springsecurityeasybytes/configuration/SpringSecurityUsernameAndPasswordAuthenticationProvider.java
+++ src/main/java/com/williamfeliciano/springsecurityeasybytes/configuration/SpringSecurityUsernameAndPasswordAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package com.williamfeliciano.springsecurityeasybytes.configuration;
 
+import com.williamfeliciano.springsecurityeasybytes.models.Authority;
 import com.williamfeliciano.springsecurityeasybytes.models.Customer;
 import com.williamfeliciano.springsecurityeasybytes.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class SpringSecurityUsernameAndPasswordAuthenticationProvider implements AuthenticationProvider {
@@ -42,10 +44,7 @@ public class SpringSecurityUsernameAndPasswordAuthenticationProvider implements 
             // Check if the given password when hashed with password encoder is the same as the user's
             // password from the db
             if(passwordEncoder.matches(pwd, customer.get(0).getPwd())){
-                // Create the authorities for the authenticated user
-                List<GrantedAuthority> authorities = new ArrayList<>();
-                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
-                return new UsernamePasswordAuthenticationToken(username,pwd,authorities);
+                return new UsernamePasswordAuthenticationToken(username,pwd,getGrantedAuthorities(customer.get(0).getAuthorities()));
             }else{
                 // The password did not match
                 throw new BadCredentialsException("Invalid Password");
@@ -55,6 +54,14 @@ public class SpringSecurityUsernameAndPasswordAuthenticationProvider implements 
             throw new BadCredentialsException("No user registered with this details");
         }
 
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Set<Authority> authorities) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Authority authority : authorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+        return grantedAuthorities;
     }
 
     @Override
